f7ff0748b247e7afa18330e46d2ea7ad01ec6277
tsuguchi0 <tsuguchi@nnn.or.jp>
2023-08-25 15:17:10 +0900

    デイトラJavaコース、DAY10　StreamAPIとラムダ式
    サンプルコード
    
    .setPower(0)を.setPower(2)に変更しました。
    
    "たーまーやー"のテキストを追加
    
    player.sendMessage(Files.readString(path, StandardCharsets.UTF_8));を追加
    
    player.sendMessage(Files.readString(path, StandardCharsets.UTF_8));を削除
    
    @EventHandler
      public void onJoin(PlayerJoinEvent event) {
        Player player = event.getPlayer();
        player.sendMessage("マインクラフトに参加しました！");
      }
      のコードを追加しました。

--- src/main/java/plugin/sample/main/Main.java
+++ src/main/java/plugin/sample/main/Main.java
@@ -1,10 +1,8 @@
 package plugin.sample.main;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -18,6 +16,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.FireworkMeta;
@@ -25,64 +24,70 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
 
-    private int count;
+  private int count;
 
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(this, this);
+  }
+
+  @EventHandler
+  public void onJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    player.sendMessage("マインクラフトに参加しました！");
+  }
+
+  /**
+   * プレイヤーがスニークを開始/終了する際に起動されるイベントハンドラ。
+   *
+   * @param e イベント
+   */
+  @EventHandler
+  public void onPlayerToggleSneak(PlayerToggleSneakEvent e) throws IOException {
+    // イベント発生時のプレイヤーやワールドなどの情報を変数に持つ。
+    Player player = e.getPlayer();
+    World world = player.getWorld();
+
+    List<Color> colorList = List.of(Color.RED, Color.BLUE, Color.WHITE, Color.BLACK);
+    if (count % 2 == 0) {
+      for (Color color : colorList) {
+
+        // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
+        Firework firework = world.spawn(player.getLocation(), Firework.class);
+
+        // 花火オブジェクトが持つメタ情報を取得。
+        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+
+        // メタ情報に対して設定を追加したり、値の上書きを行う。
+        // 今回は青色で星型の花火を打ち上げる。
+        fireworkMeta.addEffect(
+            FireworkEffect.builder()
+                .withColor(color)
+                .with(Type.BALL_LARGE)
+                .withFlicker()
+                .build());
+        fireworkMeta.setPower(2);
+
+        // 追加した情報で再設定する。
+        firework.setFireworkMeta(fireworkMeta);
+      }
+      Path path = Path.of("firework.txt");
+      Files.writeString(path, "たーまーやー");
+      player.sendMessage(Files.readString(path));
     }
-
-    /**
-     * プレイヤーがスニークを開始/終了する際に起動されるイベントハンドラ。
-     *
-     * @param e イベント
-     */
-    @EventHandler
-    public void onPlayerToggleSneak(PlayerToggleSneakEvent e) throws IOException {
-        // イベント発生時のプレイヤーやワールドなどの情報を変数に持つ。
-        Player player = e.getPlayer();
-        World world = player.getWorld();
-
-        List<Color> colorList = List.of(Color.RED, Color.BLUE, Color.WHITE, Color.BLACK);
-        if (count % 2 == 0) {
-            for (Color color : colorList) {
-
-                // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
-                Firework firework = world.spawn(player.getLocation(), Firework.class);
-
-                // 花火オブジェクトが持つメタ情報を取得。
-                FireworkMeta fireworkMeta = firework.getFireworkMeta();
-
-                // メタ情報に対して設定を追加したり、値の上書きを行う。
-                // 今回は青色で星型の花火を打ち上げる。
-                fireworkMeta.addEffect(
-                    FireworkEffect.builder()
-                        .withColor(color)
-                        .with(Type.BALL_LARGE)
-                        .withFlicker()
-                        .build());
-                fireworkMeta.setPower(2);
-
-                // 追加した情報で再設定する。
-                firework.setFireworkMeta(fireworkMeta);
-            }
-            Path path = Path.of("firework.txt");
-            Files.writeString(path,"たーまーやー");
-            player.sendMessage(Files.readString(path));
-        }
-        count++;
+    count++;
 
 
-    }
+  }
 
-    @EventHandler
-    public void onPlayerBedEnter(PlayerBedEnterEvent e) {
-        Player player = e.getPlayer();
-        ItemStack[] itemStacks = player.getInventory().getContents();
-        Arrays.stream(itemStacks)
-            .filter(item -> !Objects.isNull(item) && item.getMaxStackSize() == 64 && item.getAmount() < 64)
-          .forEach(item -> item.setAmount(64));
+  @EventHandler
+  public void onPlayerBedEnter(PlayerBedEnterEvent e) {
+    Player player = e.getPlayer();
+    ItemStack[] itemStacks = player.getInventory().getContents();
+    Arrays.stream(itemStacks)
+        .filter(item -> !Objects.isNull(item) && item.getMaxStackSize() == 64 && item.getAmount() < 64)
+        .forEach(item -> item.setAmount(64));
 
-        player.getInventory().setContents(itemStacks);
-    }
+    player.getInventory().setContents(itemStacks);
+  }
 }
