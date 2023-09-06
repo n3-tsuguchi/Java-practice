25963001bafcba07afa8366787b7df51f2a797fa
tsuguchi0 <tsuguchi@nnn.or.jp>
2023-09-07 07:55:16 +0900

    デイトラJavaコース、DAY10　StreamAPIとラムダ式
    サンプルコード
    
    .setPower(0)を.setPower(2)に変更しました。
    
    "たーまーやー"のテキストを追加
    
    player.sendMessage(Files.readString(path, StandardCharsets.UTF_8));を追加
    
    player.sendMessage(Files.readString(path, StandardCharsets.UTF_8));を削除
    
    @EventHandler
      public void onJoin(PlayerJoinEvent event) {
        Player player = event.getPlayer();
        player.sendMessage("マインクラフトに参加しました！");
      }
      のコードを追加しました。
    
      "たーまーやー"のテキストとコードを削除
    
      SetLevelCommand.java
      のファイルにsetLevelのソースコードを追加
    
      AllSetLevelCommand.java
      のファイルにallSetLevelのソースコードを追加
    
      config.ymlファイルの追加
      saveDefaultConfig();のソースコードをMain.javaファイルに追加
    
      38行目にイベントハンドラを追加　にわとりを出現させるソースコード
    
      43~47行目ソースコードダイアモンドブロックが出現するコードを追加

--- src/main/java/plugin/sample/main/Main.java
+++ src/main/java/plugin/sample/main/Main.java
@@ -10,7 +10,6 @@ import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -41,7 +40,11 @@ public final class Main extends JavaPlugin implements Listener {
     World world = player.getWorld();
     Location playerLocation = player.getLocation();
 
-    world.spawn(new Location(world, playerLocation.getX() + 3, playerLocation.getY(), playerLocation.getZ()), Chicken.class);
+    world.getBlockAt
+        (new Location(world,
+            playerLocation.getX() + 3,
+            playerLocation.getY(),
+            playerLocation.getZ())).setType(org.bukkit.Material.DIAMOND_BLOCK);
   }
 
   @EventHandler
